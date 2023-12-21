962cc20da1ef0a0fb7c290f8a2b1c9e9dfc2bca1
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-12-22 01:29:46 +0300

    second stage completed

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
 const port = 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
